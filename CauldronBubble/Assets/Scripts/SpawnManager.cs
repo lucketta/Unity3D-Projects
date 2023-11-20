4423a10a4befe5c7630a38f97d99aa65b893471c
Anna <lucketta@umich.edu>
2023-11-19 20:48:30 -0800

    Destroy object out of bounds and spawn in play area

--- CauldronBubble/Assets/Scripts/SpawnManager.cs
+++ CauldronBubble/Assets/Scripts/SpawnManager.cs
@@ -6,8 +6,9 @@ public class SpawnManager : MonoBehaviour
 {
     public float startDelay = 2;
     public float repeatRate = 1.5f;
-    private float spawnRangeX = 40;
-    private float spawnPosY = 50;
+    private float spawnRangeLeft = -38;
+    private float spawnRangeRight = 30;
+    private float spawnPosY = 80;
     private float spawnPosZ = -2;
 
     public GameObject[] foodPrefabs;
@@ -29,7 +30,7 @@ public class SpawnManager : MonoBehaviour
         int foodIndex = Random.Range(0, foodPrefabs.Length);
 
         // calculate a spawnposition thats random along the top of the screen
-        Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), spawnPosY, spawnPosZ);
+        Vector3 spawnPos = new Vector3(Random.Range(spawnRangeLeft, spawnRangeRight), spawnPosY, spawnPosZ);
         // spawn the actor
         Instantiate(foodPrefabs[foodIndex], spawnPos, transform.rotation);
     }
